9ea2928432b4c83d6519c30861a67ff6cdd8f5bc
Jordan Pitlor <jordan@pitlor.dev>
2025-12-01 21:33:13 -0500

    initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Traffic Watcher integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Traffic Watcher tests."""
+from custom_components.traffic_watcher.const import (
+    CONF_PASSWORD,
+)
+from custom_components.traffic_watcher.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
